eaae02a0e29039a1d21b207d39aad2968f65ab06
Danilo Gama <danilo.gama@gmail.com>
2024-04-23 09:37:36 -0300

    refactor: increase password size

--- sql/tables/User.sql
+++ sql/tables/User.sql
@@ -2,6 +2,6 @@ CREATE TABLE User (
     id          int auto_increment primary key,
     email       varchar(120) not null unique,
     name        varchar(120) not null,
-    password    varchar(20) not null,
+    password    varchar(80) not null,
     createdAt   timestamp default current_timestamp()
 ) ENGINE=INNODB;
